0e9b43e3373ea5f99572b1b31f245750ef3883a0
Dimitri Marinho <dimitri.santana.marinho@gmail.com>
2019-04-16 17:44:22 -0300

    Resolvendo Problemas (Set)

---
+++ [Set]/K - Quem pode embarcar? (Set)/Quem_pode_embarcar.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<set>
+using namespace std;
+int main(){
+    int N, infectado;
+    string nome;
+    set<string> contaminados;
+    cin >> N;
+    for(int i = 0; i < N; i++){
+        cin >> infectado >> nome;
+        if(infectado == 1)
+            contaminados.insert(nome);
+    }
+    for(set<string> :: iterator it=contaminados.begin(); it != contaminados.end(); it++)
+        cout << *it << endl;
+}
