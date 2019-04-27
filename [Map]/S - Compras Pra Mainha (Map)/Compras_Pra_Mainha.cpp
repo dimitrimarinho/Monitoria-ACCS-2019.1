5a958eb7b4bbc731dbb51719c853d120e92c4d92
Dimitri Marinho <dimitri.santana.marinho@gmail.com>
2019-04-27 12:22:44 -0300

    Problema S aceito

---
+++ [Map]/S - Compras Pra Mainha (Map)/Compras_Pra_Mainha.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<map>
+using namespace std;
+int main(){
+    int N, P, L, valor_final = 0;
+    string produto;
+    map<string, int> compras;
+    cin >> N;
+    for(int i = 0; i < N; i++){
+        cin >> produto >> P;
+        compras[produto] = P;
+    }
+    cin >> L;
+    for(int i = 0; i < L; i++){
+        cin >> produto;
+        valor_final += compras[produto];
+    }
+    cout << valor_final << endl;
+}
