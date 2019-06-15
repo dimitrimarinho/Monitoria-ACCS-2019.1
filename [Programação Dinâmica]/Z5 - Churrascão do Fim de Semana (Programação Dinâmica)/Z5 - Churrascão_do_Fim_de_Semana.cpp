320d9b17cd9d9adc3178690782005344aa366a45
Dimitri Marinho <dimitri.santana.marinho@gmail.com>
2019-06-15 12:14:25 -0300

    Removendo comentários

--- [Programação Dinâmica]/Z5 - Churrascão do Fim de Semana (Programação Dinâmica)/Z5 - Churrascão_do_Fim_de_Semana.cpp
+++ [Programação Dinâmica]/Z5 - Churrascão do Fim de Semana (Programação Dinâmica)/Z5 - Churrascão_do_Fim_de_Semana.cpp
@@ -7,7 +7,7 @@ int main(){
 	int N_itens, R_dinheiro, C_custo, F_felizes, k = 1;
    	vector<pair<int,int>> feliz_custo;
 
-	cin >> N_itens >> R_dinheiro; 
+	cin >> N_itens >> R_dinheiro;
     int prog_din[R_dinheiro+1];
 	feliz_custo.push_back(pair<int,int>(-1,-1));
 
@@ -20,30 +20,13 @@ int main(){
 	    prog_din[i] = 0;
 
 	for(int i = 1; i <= N_itens; i++) {
-        // cout << endl << "i = " << i << endl;
 	    for(int j = R_dinheiro; j >= 0; j--) {
-            // cout << endl << "j = " << j << endl;
 		    if(j >= feliz_custo[i].first) {
-                // cout << endl << "(antes max) prog_din[j] = " << prog_din[j] << endl;
     			prog_din[j] = max<int>(prog_din[j], prog_din[j-feliz_custo[i].first] + feliz_custo[i].second);
-                // cout << endl << "prog_din[j] = " << prog_din[j] << endl;
-                // cout << endl << "feliz_custo[i].first = " << feliz_custo[i].first << endl;
-                // cout << endl << "feliz_custo[i].second = " << feliz_custo[i].second << endl;
-                // cout << endl << "prog_din[j-feliz_custo[i].first] = " << prog_din[j-feliz_custo[i].first] << endl;
-                // cout << endl << "prog_din[j-feliz_custo[i].first] + feliz_custo[i].second = " << prog_din[j-feliz_custo[i].first] + feliz_custo[i].second << endl;
 			} else
-                // cout << endl << "else: prog_din[j] = " << prog_din[j]  << endl;
 	    		prog_din[j] = prog_din[j];
 		}
 	}
-
-    /* debug: prog_din
-
-    for(int i = 0; i < (R_dinheiro + 1); i++)
-        cout << prog_din[i] << " ";
-    cout << endl;
-   
-    */
 
 	cout << prog_din[R_dinheiro] << endl;
     
